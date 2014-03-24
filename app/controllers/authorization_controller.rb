b76980630f07ed22fe15276934917082d08c3094
Tanner Swenson <TannerSwenson@gmail.com>
2014-03-24 11:40:04 -0700

    Added sign up removing all matching system

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -3,7 +3,7 @@ class AuthorizationController < WebsocketRails::BaseController
     # The channel name will be passed inside the message Hash
     channel = WebsocketRails[message[:channel]]
 
-    if user_signed_in? && current_user.email == message[:channel]
+    if user_signed_in? && "user/" + current_user.id == message[:channel]
       accept_channel current_user.player
     else
       deny_channel({:message => 'authorization failed!'})
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,10 @@
 class SiteController < ApplicationController
   def index
+    if user_signed_in?
+      @current_user = current_user
+    else
+      redirect_to new_user_session_path
+    end
+
   end
 end
